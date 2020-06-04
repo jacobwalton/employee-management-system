495f14ad6d872eaf5ecb18eb158626f9f4201fd8
Jacob Walton <jawalton6616@gmail.com>
2020-06-03 21:25:49 -0500

    set up requirements

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employees",
+});
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
