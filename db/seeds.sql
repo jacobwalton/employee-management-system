ca541e3f3be204d798ff7c90d915fd3a535670ab
Jacob Walton <jawalton6616@gmail.com>
2020-06-03 21:59:55 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,28 @@
 use employees;
 
-INSERT INTO department
-    (name)
+INSERT INTO department (name)
 VALUES
-    ('Sales Department'),
-    ('Engineering Department'),
-    ('Accounting Department'),
-    ('Legal Department');
+    ("Sales Department"),
+    ("Engineering Department"),
+    ("Accounting Department"),
+    ("Legal Department");
 
-INSERT INTO role
-    (title, salary, department_id)
+INSERT INTO role (job_title, salary, department_id)
 VALUES
-    ('Sales Lead', 60000, 01),
-    ('Salesperson', 50000, 01),
-    ('Lead Engineer', 90000, 02),
-    ('Software Engineer', 60000, 02),
-    ('Account Manager', 80000, 03),
-    ('Accountant', 90000, 03),
-    ('Legal Advisor', 100000, 04),
-    ('Lawyer', 140000, 04);
+    ("Sales Lead", 60000, 01),
+    ("Salesperson", 50000, 01),
+    ("Lead Engineer", 90000, 02),
+    ("Engineer", 60000, 02),
+    ("Account Manager", 80000, 03),
+    ("Accountant", 90000, 03),
+    ("Legal Advisor", 100000, 04),
+    ("Lawyer", 140000, 04);
 
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ("Tim", "Calhoon", 1, 1),
+    ("Joe", "Carter", 2, 2),
+    ("Marge", "Westbrook", 3, 2),
+    ("Fred", "Rice", 4, 4),
+    ("Sam", "King", 5, 1),
+    ("Jim", "Vance", 6, 4);
